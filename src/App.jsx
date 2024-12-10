21457ccb90ffca024bc75025c9a1d3d1ccd80b63
sggyflt <sgflt88@gmail.com>
2024-12-10 15:35:18 +0100

    feat: add custom Tailwind colors

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
 
   return (
     <div>
-     <h1 className='font-urbanist text-8xl'>Test Urbanist</h1>
-     <span className='text-4xl font-inter'>Test Inter</span>
+     <h1 className='font-urbanist text-8xl text-flamingo-400'>Test Flamingo Color</h1>
+     <span className='text-4xl font-inter text-midnight-400'>Test Midnight Color</span>
+     <h2 className='text-2xl text-grey-400'>Test Grey Color</h2>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'flamingo': {
+        100: '#FFF8FB',
+        200: '#FEDDEA',
+        300: '#FF0054',
+        400: '#e30e54',
+      },
+      'midnight': {
+        300: '#900461',
+        400: '#26165d',
+      },
+      'grey': {
+        50: '#FFFFFF', /** white */
+        200: '#D1CFD8', /** light grey */
+        300: '#6E6E6E', /** dark grey */
+        400: '#222222', /** black */
+      }
+    },
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
