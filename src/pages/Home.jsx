4be2dff605489280c548d042d19ecfd907bef9d7
sggyflt <sgflt88@gmail.com>
2024-12-11 18:11:38 +0100

    feat: add Come Funziona section markup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,9 +54,57 @@ function Home() {
       </div>
       <div>
         {/*Come Funziona*/}
-        <Heading level={2} className="text-midnight-400 text-center text-[64px]">Come Funziona</Heading>
-        <Button label="Ingrosso bevande e alimentare" href="#" className="px-8 py-4 mx-16 my-8 font-normal border-2 bg-flamingo-200 hover:bg-grey-50 text-grey-400 border-grey-400" />
-        <Button label="Prodotti in assortimento" href="#" className="px-8 py-4 mx-16 my-8 font-normal border-2 bg-flamingo-200 hover:bg-grey-50 text-grey-400 border-grey-400" />
+        <section className='grid gap-4 md:grid-cols-2 py-14'>
+          <div>
+            <Heading level={2} className="text-midnight-400 text-[64px] mb-8">Come Funziona</Heading>
+            <ol className='flex flex-col gap-10'>
+              <li className='flex items-center gap-6'>
+                <div className='flex justify-center items-center min-w-12 min-h-12 border-[0.5px] rounded-full text-midnight-300 bg-flamingo-100 border-flamingo-200'>
+                  <span className='text-2xl'>1</span>
+                </div>
+                <p className='text-xl text-grey-300'>{"Ti registri al sito (bastano 20 secondi!!) e consulti subito"} <span className='font-bold'>assortimento</span> di prodotti e <span className='font-bold'>prezzi</span></p>
+              </li>
+              <li className='flex items-center gap-6'>
+                <div className='flex justify-center items-center min-w-12 min-h-12 border-[0.5px] rounded-full text-midnight-300 bg-flamingo-100  border-flamingo-200'>
+                  <span className='text-2xl'>2</span>
+                </div>
+                <p className='text-xl text-grey-300'>Ordini online 24/7, <span className='font-bold'>quando ti è comodo</span>, senza andare fisicamente all’ingrosso o aspettare un agente</p>
+              </li>
+              <li className='flex items-center gap-6'>
+                <div className='flex justify-center items-center min-w-12 min-h-12 border-[0.5px] rounded-full text-midnight-300 bg-flamingo-100  border-flamingo-200'>
+                  <span className='text-2xl'>3</span>
+                </div>
+                <p className='text-xl text-grey-300'>Ricevi l’ordine al tuo locale <span className='font-bold'>entro due giorni</span> e puoi anche<span className='uppercase'> Pagare alla consegna!!</span></p>
+              </li>
+            </ol>
+          </div>
+          <div>
+            <img src="./src/assets/images/backgrounds/how-it-works.png" alt="" />
+          </div>
+        </section>
+        
+        <section className='grid gap-5 py-16 rounded-[48px] md:grid-cols-3 bg-flamingo-100'>
+          <div className='flex flex-col items-center gap-5'>
+            <p className='text-3xl font-light font-urbanist text-midnight-400'><span className='font-bold'>Consegna</span> gratis in</p>
+            <span className='text-6xl font-inter text-flamingo-300'>48H</span>
+          </div>
+          
+          <div className='flex flex-col items-center gap-5'>
+            <p className='text-3xl font-light font-urbanist text-midnight-400'><span className='font-bold'>Ordine</span> minimo solo</p>
+            <span className='text-6xl font-inter text-flamingo-300'>100€</span>
+          </div>
+
+          <div className='flex flex-col items-center gap-5'>
+            <p className='text-3xl font-bold font-urbanist text-midnight-400'>Ordina online</p>
+            <span className='text-6xl font-inter text-flamingo-300'>24/7</span>
+          </div>
+        </section>
+
+        <section className='flex gap-10 mx-8 mt-16 mb-8 place-content-center'>
+          <Button label="Ingrosso bevande e alimentare" href="#" className="px-8 py-4 font-normal border-2 bg-flamingo-200 hover:bg-grey-50 text-grey-400 border-grey-400" />
+          <Button label="Prodotti in assortimento" href="#" className="px-8 py-4 font-normal border-2 bg-flamingo-200 hover:bg-grey-50 text-grey-400 border-grey-400" />
+        </section>
+        
       </div>
       <div>
         {/*In offerta questo mese*/}
