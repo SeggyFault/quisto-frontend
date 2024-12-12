72fce5904c45ccbe66128e668a34deaf92f2e208
sggyflt <sgflt88@gmail.com>
2024-12-12 13:27:10 +0100

    fix: FullScreenLayout styles for mobile

--- src/layout/FullScreenLayout.jsx
+++ src/layout/FullScreenLayout.jsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 const FullScreenLayout = ({ children }) => {
   return (
     <div className="max-w-[1140px] mx-auto">
-      <header className="flex pb-10 pt-14">
+      <header className="flex justify-center px-5 md:pb-10 pt-14 md:px-10 md:justify-start">
         <a href="/">
           <img src="./src/assets/images/logos/logo.svg" width="135" height="45" alt="quisto logo" />
         </a>
